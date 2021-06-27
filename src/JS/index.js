b58655233f3321ae18c366e936930f70fc3d6576
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-06-28 01:42:55 +0300

    debounce feature added

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,18 +1,26 @@
 import '../sass/main.scss';
 import fetchCountries from './fetchCountries';
 import countryList from '../teplate/country-list';
+var debounce = require('lodash.debounce');
 
 const refs = {
   searchInput: document.querySelector('.search-input'),
   countryContainer: document.querySelector('.js-countries'),
 };
 
-refs.searchInput.addEventListener('input', e => {
-  const inputValue = e.target.value;
-  fetchCountries(inputValue).then(render);
-});
+refs.searchInput.addEventListener(
+  'input',
+  debounce(e => {
+    const inputValue = e.target.value;
+    fetchCountries(inputValue).then(render);
+  }, 500),
+);
 
 function render(countries) {
   const countryMarkup = countryList(countries);
   refs.countryContainer.innerHTML = countryMarkup;
+}
+
+function error(err) {
+  console.log(err);
 }
